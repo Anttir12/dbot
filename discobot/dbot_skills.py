5e949649eab4a7d1e35894035feda32c97236608
Antti <antti.ruohisto@gmail.com>
2020-10-31 00:01:23 +0200

    Sound clip preview and cleaning
    Sound clips now utilise the CachedStream to make the preview spamming more efficient.
    Also did a bunch of small cleaning

--- discobot/dbot_skills.py
+++ discobot/dbot_skills.py
@@ -69,17 +69,18 @@ class DBotSkills:
             logger.info("voice_client NOT found")
 
     async def play(self, yt_url: str):
-        source, title = await self._get_stream(yt_url)
-        if source:
+        cached_stream = await sync_to_async(utils.get_stream)(yt_url)
+        if cached_stream:
+            source = cached_stream.file.path
             if self.guild.voice_client:
                 if self.channel:
-                    self.channel.send("Now playing {}".format(title))
+                    self.channel.send("Now playing {}".format(cached_stream.title))
                 audio = PCMVolumeTransformer(FFmpegPCMAudio(source), self.volume)
                 self.guild.voice_client.play(audio)
             else:
                 logger.info("Unable to find voice client")
         else:
-            logger.info("No source: {}".format(source))
+            logger.info("Could not find stream")
             if self.channel:
                 await self.channel.send("I was unable to find a proper stream. Sorry!")
 
@@ -87,51 +88,3 @@ class DBotSkills:
         cid = ctx.message.channel.id
         self.channel = ctx.message.channel
         await ctx.message.channel.send(f"Channel ID: {cid}")
-
-    async def _get_stream(self, yt_url):
-        yt_id = await self._get_yt_id_from_url(yt_url)
-        cached_stream_query = await sync_to_async(CachedStream.objects.filter)(yt_id=yt_id)
-        cached_stream: CachedStream = await sync_to_async(cached_stream_query.first)()
-        if cached_stream:
-            source = cached_stream.file.path
-            title = cached_stream.title
-            logger.info("{} found in cache. Skipping download".format(yt_id))
-        else:
-            source, title = await self._download_stream_and_cache_it(yt_url, yt_id)
-        return source, title
-
-    async def _download_stream_and_cache_it(self, yt_url, yt_id, cache_it=True):
-        try:
-            y_t = pytube.YouTube(yt_url)
-        except Exception as broad_except:  # pylint: disable=broad-except
-            raise SkillException("Unable to load streams from {}".format(yt_url)) from broad_except
-
-        filtered = y_t.streams.filter(audio_codec="opus").order_by('bitrate').desc().first()
-        if not utils.enough_disk_space_for_yt_stream(filtered, "/tmp"):
-            raise SkillException("Not enough disk space to download yt stream")
-        source = filtered.download("/tmp/streams")
-        title = filtered.title
-        size = os.path.getsize(source)
-        if cache_it:
-            with open(source, 'rb') as ytaudio:
-                ytaudio.seek(0)
-                mime_type = magic.from_buffer(ytaudio.read(1024), mime=True)
-                ytaudio.seek(0)
-                file = InMemoryUploadedFile(ytaudio, "sound_effect", title, mime_type, size, None)
-                cached_stream = CachedStream(title=title, yt_id=yt_id, file=file, size=filtered.filesize_approx)
-                await sync_to_async(cached_stream.save)(remove_oldest_if_full=True)
-                temp_source = source
-                source = cached_stream.file.path
-                os.remove(temp_source)
-        return source, title
-
-    async def _get_yt_id_from_url(self, yt_url):
-        parsed_url = urlparse(yt_url)
-        if yt_url.startswith("https://www.youtube.com/"):
-            yt_id = parse_qs(parsed_url.query)["v"][0]
-        elif yt_url.startswith("https://youtu.be/"):
-            yt_id = parsed_url.path[1:]
-        else:
-            raise SkillException("URL not valid. URL has to start with <https://www.youtube.com/> or "
-                                 "<https://youtu.be/>")
-        return yt_id
