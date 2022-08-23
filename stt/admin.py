8f72d908f682986b551efc93cedf8ec165d5be85
Antti <antti.ruohisto@gmail.com>
2022-08-23 18:49:55 +0300

    Better multi select thing for SttReaction admin form

--- stt/admin.py
+++ stt/admin.py
@@ -1,10 +1,25 @@
+from django import forms
 from django.contrib import admin
+from django.contrib.admin.widgets import FilteredSelectMultiple
 
 # Register your models here.
 from stt.models import SttReaction
+from sounds import models as sound_models
+
+
+class SttReactionForm(forms.ModelForm):
+
+    sound_effects = forms.ModelMultipleChoiceField(queryset=sound_models.SoundEffect.objects.all(),
+                                                   widget=FilteredSelectMultiple("Sounds", is_stacked=False))
+
+    class Meta:
+        model = SttReaction
+        fields = ("name", "active", "cooldown", "data", "sound_effects")
 
 
 class SttReactionAdmin(admin.ModelAdmin):
+    form = SttReactionForm
     pass
+
 
 admin.site.register(SttReaction, SttReactionAdmin)
